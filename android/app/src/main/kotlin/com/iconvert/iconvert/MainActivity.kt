ad57afbb390347aec90478ebaddad659892a281f
明 <czm1763470842@gmail.com>
2026-07-04 16:53:56 +0800

    fix: 简化 MainActivity（flutter_foreground_task 6.x 用自动注册，不需要手动 addTaskObserver）

--- android/app/src/main/kotlin/com/iconvert/iconvert/MainActivity.kt
+++ android/app/src/main/kotlin/com/iconvert/iconvert/MainActivity.kt
@@ -1,18 +1,5 @@
 package com.iconvert.iconvert
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import com.pravera.flutter_foreground_task.FlutterForegroundTaskPlugin
 
-class MainActivity : FlutterActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        // 注册 flutter_foreground_task 的 background service
-        FlutterForegroundTaskPlugin.addTaskObserver(flutterEngine)
-    }
-
-    override fun cleanUpFlutterEngine(flutterEngine: FlutterEngine) {
-        FlutterForegroundTaskPlugin.removeTaskObserver(flutterEngine)
-        super.cleanUpFlutterEngine(flutterEngine)
-    }
-}
+class MainActivity : FlutterActivity()
