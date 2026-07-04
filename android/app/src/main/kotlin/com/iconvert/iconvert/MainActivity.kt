b21b6bf4fe2dd61b604d1121cb957bc27a396014
明 <czm1763470842@gmail.com>
2026-07-04 15:05:25 +0800

    feat: MainActivity 注册 FlutterForegroundTaskPlugin

--- android/app/src/main/kotlin/com/iconvert/iconvert/MainActivity.kt
+++ android/app/src/main/kotlin/com/iconvert/iconvert/MainActivity.kt
@@ -1,5 +1,18 @@
 package com.iconvert.iconvert
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import com.pravera.flutter_foreground_task.FlutterForegroundTaskPlugin
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        // 注册 flutter_foreground_task 的 background service
+        FlutterForegroundTaskPlugin.addTaskObserver(flutterEngine)
+    }
+
+    override fun cleanUpFlutterEngine(flutterEngine: FlutterEngine) {
+        FlutterForegroundTaskPlugin.removeTaskObserver(flutterEngine)
+        super.cleanUpFlutterEngine(flutterEngine)
+    }
+}
